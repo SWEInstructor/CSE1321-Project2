01db6145483bf21d3730924dce710760009d0ca8
Rande147 <90401561+Rande147@users.noreply.github.com>
2021-10-24 22:55:55 -0400

    Update Home.java

--- Project2/src/main/Home.java
+++ Project2/src/main/Home.java
@@ -27,29 +27,28 @@ public class Home {
 
 			if (command== "use") {
 				System.out.println("What are you gonna use " + NAME + "?");
-				String interactWith;
-				
+
 				if (!(Inventory.hasItem("libraryPass"))) {
 					String[] houseObjects = {"book","record player","plastic bowl","tv","dead plant","library card","cave passage"};
-					interactWith= TextGame.checkInput(houseObjects);
+					command= TextGame.checkInput(houseObjects);
 				} else if (Inventory.hasItem("libraryPass")) {
 					String[] houseObjects =  {"book","record player","plastic bowl","tv","dead plant","cave passage"};
-					interactWith= TextGame.checkInput(houseObjects);
+					command= TextGame.checkInput(houseObjects);
 				}
 
-				if (interactWith== "cave passage") {
+				if (command== "cave passage") {
 					place= "cave";
-				} else if (interactWith== "book") {
+				} else if (command== "book") {
 					System.out.println(NAME + " picks up the book and opens it... Chapters 4-8 fall out of the book :/");
-				} else if (interactWith== "record player") {
+				} else if (command== "record player") {
 					System.out.println(NAME + " presses play on the record player and is rick rolled.");
-				} else if (interactWith== "plastic bowl") {
+				} else if (command== "plastic bowl") {
 					System.out.println(NAME + " picks up the plastic bowl and then sets it back down.");
-				} else if (interactWith== "tv") {
+				} else if (command== "tv") {
 					System.out.println(NAME + " turns on the tv and Spongebob appears on the screen!");
-				} else if (interactWith== "dead plant") {
+				} else if (command== "dead plant") {
 					System.out.println(NAME + " smells the dead plant... Why " + NAME + " is doing this I have no clue");
-				} else if (interactWith== "library card") {
+				} else if (command== "library card") {
 					if (!(Inventory.hasItem("library card"))) {
 						System.out.println(NAME + " has picked up the library card.");
 						Inventory.addItemToInv("libraryPass", NAME);

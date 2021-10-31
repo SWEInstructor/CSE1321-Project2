11dbeb3724a8c09f87e9a6cec8f8131de0fccf36
TheGameKid <90401564+TheGameKid@users.noreply.github.com>
2021-10-31 17:24:32 -0400

    Update Cave.java

--- Project2/src/main/Cave.java
+++ Project2/src/main/Cave.java
@@ -103,3 +103,15 @@ public class Cave {
 		}			
 	}
 }
+public class Test1 {
+
+	static String NAME = "a";
+	static  String PLACE = "cave";
+	
+    static  String[] input = {"use","look","visit","check inventory","help"};
+    static  String command = TextGame.checkInput(input);
+	public static void main(String[] args) {
+		
+		Cave.cave(NAME, PLACE, command, input);
+	
+	}

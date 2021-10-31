d76002e6b0ffa721632502463ab02faa60b998be
jalen941 <71603490+jalen941@users.noreply.github.com>
2021-10-31 16:36:12 -0400

    Update TownSquare.java

--- Project2/src/main/TownSquare.java
+++ Project2/src/main/TownSquare.java
@@ -1,47 +1,16 @@
 package main;
 
-public class TownSquare {
+public class Townsquaretest {
+public static void main (String args[]) {
 	
-	public static void townSquare(String NAME, String place, String command, String[] inputs)
-	{
-		if (place.equals("townSquare")){
-			System.out.println("You walk into the town square and see a farmer's stand and a flower stall.");
-			System.out.println("So, what are you doing now?: ");
-
-			command = TextGame.checkInput(inputs);
-
-			if (command.equals("visit")){
-				TextGame.visit(place);
-			}
-
-			if (command.equals("help")) {
-				TextGame.help();
-			}
-
-			if (command.equals("look"))  {
-				System.out.println("Oh yeah, you can see your friend's house from here.");
-				System.out.println("But I already said, there's a flower stall and a farmer's stand here!");
-			}
-
-			if (command.equals("use"))   {
-				String action1;
-				System.out.println("Which one would you like to go to?: ");
-				String[] squareOptions = {"farmer's stand", "flower stall", "friend's house"};
-				action1 = TextGame.checkInput(squareOptions);
-				if (action1 == "farmer's stand")
-				{
-					System.out.println("You see the fruits and veggies the stand has but you're too broke to afford any of it.");
-				} else if (action1 == "flower stall") {
-					System.out.println("You look at all the different flowers at the stand. " + NAME + " should probably get their mom some flowers if they ever find the treasure.");
-				} else if (action1 == "friend's house") {
-					place = "friendsHouse";
-				}
-		      	}
-
-		      	if (command == "check inventory")   {
-				Inventory.checkInv(NAME);
-	    		}
-
-	    	}
-	}
+	String[] inputs = {"use","look","visit","check inventory","help"};
+	String command = "use";
+	String place = "townSquare";
+	String name = "jalen" ;
+	TownSquare.townSquare("jalen" , "townSquare", "use", inputs);
+	
+	
+	
+	
+}
 }

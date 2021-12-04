6fc3c5a5c0cc37307842f1f3c8024567ad243d31
cdow505 <90405877+cdow505@users.noreply.github.com>
2021-12-04 18:19:17 -0500

    Update TextgameTest.java
    Removed checkInput because it can't be properly tested with junit, as it uses a scanner.

--- Project2/src/main/TextgameTest.java
+++ Project2/src/main/TextgameTest.java
@@ -6,20 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class TextgameTest {
 
-	@Test
-	void testCheckInput() {
-		Textgame myObject = new Textgame();
-		String[] inputs = {"use", "look", "check inventory", "help", "visit"};
-		String result = myObject.checkInput(inputs);
-		boolean correct = false;
-		for(int i =0; i<inputs.length; i++) {
-			if(inputs[i].equals(result)) {
-				correct =true;
-			}
-		}
-		assertEquals(true, correct);
-	}
-
 
 	@Test
 	void testGetName() {
